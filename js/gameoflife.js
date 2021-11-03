79b8632217f3355c9e2e8a496b2bbb444f9b8ff9
vance443 <vancesoita2014@gmail.com>
2021-11-03 13:35:36 +0300

    Completed module

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -44,11 +44,13 @@ const printCells = (state) => {
   return accumulator;
 };
 
-const getNeighborsOf = ([x, y]) => [
-  [x-1, y+1], [x, y+1], [x+1, y+1],
-  [x-1, y],            [x+1, y],
-  [x-1, y-1], [x, y-1], [x=1, y-1]
-];
+const getNeighborsOf = ([x, y]) => {
+
+    [x - 1, y + 1], [x, y + 1], [x + 1, y + 1],
+    [x - 1, y], [x + 1, y],
+    [x - 1, y - 1], [x, y - 1], [x = 1, y - 1]
+  
+};
 
 const getLivingNeighbors = (cell, state) => {
   return getNeighborsOf(cell).filter((n) => contains.bind(state)(n));
